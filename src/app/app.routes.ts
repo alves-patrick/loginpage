03090737f29b02ad742d98bbe99cda0faa75f809
“Patrick <patrickalvesarts@gmail.com>
2025-06-14 22:01:23 -0300

    Rotas, login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login'; // Adjust the import path as necessary
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+
+       path: "login",
+       component: Login // Assuming you have a LoginComponent defined
+
+
+    }
+
+
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DefaultLoginLayout } from './components/default-login-layout/default-login-layout';
 
 @Component({
   selector: 'app-root',
